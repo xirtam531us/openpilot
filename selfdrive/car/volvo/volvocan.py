77eef6ae4b11c6ff65e7b69c13f4e66a9f257c3d
Comma Device <device@comma.ai>
2020-04-10 17:07:39 +0000

    Cleanup and new signals to EUCD.
    carcontroller.py - Cleanup
    carstate.py - New signals
    volvocan.py - Manipulate servo here now instead of carcontroller.py

--- selfdrive/car/volvo/volvocan.py
+++ selfdrive/car/volvo/volvocan.py
@@ -1,5 +1,27 @@
 from selfdrive.car.volvo.values import CAR
 
+def manipulateServo(packer, car_fingerprint, CS, can_sends):
+  # Manipulate data from servo to FSM
+  # Zero active and torque bits.
+  msg = {
+      "LKAActive" : (CS.PSCMInfo.LKAActive & 0xFD),
+      "LKATorque" : 0,
+      "SteeringAngleServo" : CS.PSCMInfo.SteeringAngleServo,
+      "byte0" : CS.PSCMInfo.byte0,
+      "byte4" : CS.PSCMInfo.byte4,
+      "byte7" : CS.PSCMInfo.byte7,
+  }
+
+  if car_fingerprint == CAR.V40: 
+    msg["byte3"] = CS.PSCMInfo.byte3
+  elif car_fingerprint == CAR.V60:
+    msg["SteeringWheelRateOfChange"] = CS.PSCMInfo.SteeringWheelRateOfChange
+
+  can_sends.append(packer.make_can_msg("fromServo1", 2, msg))
+  
+  return can_sends
+
+
 def create_chksum(dat, car_fingerprint):
   # Input: dat byte array
   # Steering direction = 0 -> 3
@@ -21,13 +43,13 @@ def create_chksum(dat, car_fingerprint):
   # Checksum is inverted sum of all bytes
   return s ^ 0xFF
 
-def create_steering_control(packer, car_fingerprint, apply_steer, frame, steer_on, steer_direction, unkown, FSMInfo):  
+def create_steering_control(packer, frame, car_fingerprint, SteerCommand, FSMInfo):  
  
   # Set common parameters
   values = {
-    "LKAAngleRequest": apply_steer,
-    "LKADirection": steer_direction,
-    "Unkown": unkown,
+    "LKAAngleRequest": SteerCommand.angle_request,
+    "LKADirection": SteerCommand.steer_direction,
+    "Unkown": SteerCommand.unkown,
   }
   
   # Set car specific parameters
