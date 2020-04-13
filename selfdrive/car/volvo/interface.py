61e840598359e32315b293ae704ba17562d94acf
Comma Device <device@comma.ai>
2020-04-13 19:24:22 +0000

    Alot of small changes to all volvo files.
    Minor changes volvo interface.py, tuning parameters
    
    Volvo C1 fingerprinting 2.0, not tested
    
    Volvo carcontroller.py and interface.py cleanup.
    
    Cleanup volvo interface.py
    
    Cleanup volvo values.py
    
    Updated carcontroller.py and values.py, sending data in unknown to EPS now also as a test.

--- selfdrive/car/volvo/interface.py
+++ selfdrive/car/volvo/interface.py
@@ -3,7 +3,7 @@ from cereal import car
 from common.params import Params
 from selfdrive.config import Conversions as CV
 from selfdrive.controls.lib.drive_helpers import create_event, EventTypes as ET
-from selfdrive.car.volvo.values import CAR, BUTTON_STATES
+from selfdrive.car.volvo.values import CAR, PLATFORM, BUTTON_STATES
 from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint
 from selfdrive.car.interfaces import CarInterfaceBase
 
@@ -24,33 +24,30 @@ class CarInterface(CarInterfaceBase):
   def get_params(candidate, fingerprint=gen_empty_fingerprint(), has_relay=False, car_fw=[]):
     ret = CarInterfaceBase.get_std_params(candidate, fingerprint, has_relay)
     
-    ret.radarOffCan = True  # No radar objects on can
-    
-    if candidate == CAR.V40 or candidate == CAR.V60:
-      # Set specific paramter
-      if candidate == CAR.V40:
-        ret.safetyParam = 1
-        ret.safetyModel = car.CarParams.SafetyModel.volvoC1
-      if candidate == CAR.V60:
-        ret.safetyParam = 2
-        ret.safetyModel = car.CarParams.SafetyModel.volvoEUCD
-      # Set common parameters
+    # Set specific paramter
+    if candidate in PLATFORM.C1:
+      ret.safetyParam = 1
+      ret.safetyModel = car.CarParams.SafetyModel.volvoC1
+    if candidate in PLATFORM.EUCD:
+      ret.safetyParam = 2
+      ret.safetyModel = car.CarParams.SafetyModel.volvoEUCD
+   
+    if candidate:
+       # Set common parameters
       ret.carName = "volvo"
-      ret.enableCruise = True # Stock ACC
-      ret.openpilotLongitudinalControl = False
+      ret.radarOffCan = True  # No radar objects on can
       ret.steerControlType = car.CarParams.SteerControlType.angle
-      ret.steerLimitAlert = False # Steer torque is strongly rate limit and max value is decently high. Off to avoid false positives
-      ret.minSteerSpeed = 30. * CV.KPH_TO_MS
+      #ret.steerLimitAlert = False # Do this do anything?
+      #ret.minSteerSpeed = 30. * CV.KPH_TO_MS
       ret.enableCamera = True # force openpilot to fake the stock camera
       
-      # Steering settings
-      ret.steerRateCost = 0.5
-      ret.steerActuatorDelay = 0.25
-      ret.steerLimitTimer = 0.4
-      ret.steerMaxBP = [0.] # m/s
-      ret.steerMaxV = [1.]
-
-      # No PID control used. 
+      # Steering settings - tuning parameters for lateral control.
+      ret.steerRateCost = 0.5 # Used in pathplanner for punishing? Steering movements?
+      ret.steerActuatorDelay = 0.1 # Actuator delay from input to output.
+      
+      # No PID control used. Set to 0, otherwise pid loop crashes.
+      #ret.steerMaxBP = [0.] # m/s
+      #ret.steerMaxV = [1.]
       ret.lateralTuning.pid.kpBP = [0.]
       ret.lateralTuning.pid.kiBP = [0.]
       # Tuning factors
@@ -102,36 +99,13 @@ class CarInterface(CarInterfaceBase):
     # Events 
     events = self.create_common_events(ret)
     
-    # Decide when to activate / deactivate openpilot
-    if ret.seatbeltUnlatched:
-      events.append(create_event('seatbeltNotLatched', [ET.NO_ENTRY, ET.SOFT_DISABLE]))
-
-    if ret.doorOpen:
-      events.append(create_event('doorOpen', [ET.NO_ENTRY, ET.SOFT_DISABLE]))
-    
-    # Engage when ACC goes on if the speed is above 70 km/h. 
-    # If ACC is already engaged, only engage openpilot when ACC is on 
-    # and above 70 km/h and a rising edge appears on +,- or resume button.
-    if ret.cruiseState.enabled and not self.cruiseState_enabled_prev and ret.vEgo > self.CP.minEnableSpeed \
-       or ret.cruiseState.enabled and ret.vEgo > self.CP.minEnableSpeed \
-       and (self.CS.buttonStates['accelCruise'] and not self.buttonStatesPrev['accelCruise'] \
-       or self.CS.buttonStates['decelCruise'] and not self.buttonStatesPrev['decelCruise'] \
-       or self.CS.buttonStates['resumeCruise'] and not self.buttonStatesPrev['resumeCruise']):
-      events.append(create_event('pcmEnable', [ET.ENABLE]))
-    
-    if not ret.cruiseState.enabled or ret.brakePressed:
+    # Engagement and longitudinal control using stock ACC. Make sure OP is
+    # disengaged if stock ACC is disengaged.
+    if not ret.cruiseState.enabled:
       events.append(create_event('pcmDisable', [ET.USER_DISABLE]))
-
-    if ret.gasPressed:
-      events.append(create_event('pedalPressed', [ET.PRE_ENABLE]))
-    
-    # low speed steer alert hysteresis logic (only for cars with steer cut off above 10 m/s)
-    if ret.vEgo < (self.CP.minSteerSpeed + 1.) and self.CP.minSteerSpeed > 10.:
-      self.low_speed_alert = True
-    if ret.vEgo > (self.CP.minSteerSpeed + 2.):
-      self.low_speed_alert = False
-    if self.low_speed_alert:
-      events.append(create_event('belowSteerSpeed', [ET.WARNING]))
+    # Attempt OP engagement only on rising edge of stock ACC engagement.
+    elif not self.cruiseState_enabled_prev:
+      events.append(create_event('pcmEnable', [ET.ENABLE]))
 
     ret.events = events
     ret.buttonEvents = buttonEvents
@@ -147,6 +121,10 @@ class CarInterface(CarInterfaceBase):
     return self.CS.out
 
   def apply(self, c):
-    can_sends = self.CC.update(self.CS, self.frame, c.actuators)
+    can_sends = self.CC.update(c.enabled, self.CS, self.frame,
+                               c.actuators, c.cruiseControl.cancel,
+                               c.hudControl.visualAlert, c.hudControl.leftLaneVisible,
+                               c.hudControl.rightLaneVisible, c.hudControl.leadVisible,
+                               c.hudControl.leftLaneDepart, c.hudControl.rightLaneDepart)
     self.frame += 1
     return can_sends
