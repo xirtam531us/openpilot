901e7867303243f2c9d7933c0d8e767e74e83c09
danielzmod <33908198+danielzmod@users.noreply.github.com>
2020-07-05 14:02:43 +0200

    Update thermald.py
    Dont prompt for internet connection in a quite a while

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -26,8 +26,8 @@ NetworkType = log.ThermalData.NetworkType
 NetworkStrength = log.ThermalData.NetworkStrength
 CURRENT_TAU = 15.   # 15s time constant
 CPU_TEMP_TAU = 5.   # 5s time constant
-DAYS_NO_CONNECTIVITY_MAX = 360*10  # do not allow to engage after a week without internet
-DAYS_NO_CONNECTIVITY_PROMPT = 360*9  # send an offroad prompt after 4 days with no internet
+DAYS_NO_CONNECTIVITY_MAX = 360*20  # do not allow to engage after a week without internet
+DAYS_NO_CONNECTIVITY_PROMPT = 360*19  # send an offroad prompt after 4 days with no internet
 DISCONNECT_TIMEOUT = 5.  # wait 5 seconds before going offroad after disconnect so you get an alert
 
 LEON = False
